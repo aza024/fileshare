03507b2c9ddeb97e12855a1fc6b6a26b14cc020b
Andrea  Piazza <andrea@andreas-mbp.attlocal.net>
2018-10-10 23:10:32 -0700

    added UUID column to table to pass to JWT token. added bcrypt. auth fully functional

--- backend/public/scripts/signup.js
+++ backend/public/scripts/signup.js
@@ -14,24 +14,24 @@ var $form_modal = $('.user-modal'),
     $tab_signup = $form_modal_tab.children('li').eq(1).children('a'),  
     $main_nav = $('.main-nav');
     $form_data = $(this).serialize()
-     
+    
     $main_nav.on('click', function(e){
-      if( $(e.target).is($main_nav) ) {
-        $(this).children('ul').toggleClass('is-visible');
-      } else {
-        $main_nav.children('ul').removeClass('is-visible');
+        if( $(e.target).is($main_nav) ) {
+            $(this).children('ul').toggleClass('is-visible');
+        } else {
+            $main_nav.children('ul').removeClass('is-visible');
         //show modal layer
-        $form_modal.addClass('is-visible'); 
+            $form_modal.addClass('is-visible'); 
         //show the selected form
-        ( $(e.target).is('.signup') ) ? signup_selected() : login_selected();
-      }
+        ($(e.target).is('.signup') ) ? signup_selected() : login_selected();
+        }
     });
-  
+
     //close modal
     $('.user-modal').on('click', function(e){
         if( $(e.target).is($form_modal) || $(e.target).is('.close-form') ) {
             $form_modal.removeClass('is-visible');
-      } 
+        } 
     });
 
     $(document).keyup(function(e){
@@ -39,31 +39,31 @@ var $form_modal = $('.user-modal'),
         if(e.which=='27'){
             $form_modal.removeClass('is-visible');
         }
-      });
-  
+    });
+
     // switch tabs
     $form_modal_tab.on('click', function(e) {
         e.preventDefault();
         ( $(e.target).is( $tab_login ) ) ? login_selected() : signup_selected();
     });
-  
+
     //hide or show password
     $('.hide-password').on('click', function(){
         var $this= $(this),
         $password_field = $this.prev('input');
 
-      ( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') :  
+        ( 'password' == $password_field.attr('type') ) ? $password_field.attr('type', 'text') :  
         $password_field.attr('type', 'password');
-      ( 'Show' == $this.text() ) ? $this.text('Hide') : $this.text('Show');
+        ( 'Show' == $this.text() ) ? $this.text('Hide') : $this.text('Show');
     });
-  
+
     function login_selected(){
         $form_login.addClass('is-selected');
         $form_signup.removeClass('is-selected');
         $tab_login.addClass('selected');
         $tab_signup.removeClass('selected');
     }
-  
+
     function signup_selected(){
         $form_login.removeClass('is-selected');
         $form_signup.addClass('is-selected');
@@ -91,15 +91,12 @@ $('#signin-form').on('submit',function(e){
         },
         success: function(res){
             let token = res.token
-            console.log('TOKEN'+JSON.stringify(token))
             localStorage.setItem('User Token', token);
             localStorage.setItem('User Email', email);
-            console.log("success " + JSON.stringify(res))
         },
         error: function(res){console.log('Error:' + JSON.stringify(res))}
     })
-    
-    }
+}
 )
 
 $('#signup-form').on('submit', function(e){
@@ -117,12 +114,14 @@ $('#signup-form').on('submit', function(e){
                 email,
                 password
             },
-            success: function(){console.log("success")},
+            success: function(res){
+                let token = res.token
+                localStorage.setItem('User Name', username);
+                localStorage.setItem('User Token', token);
+                localStorage.setItem('User Email', email);
+            },
             error: function(res){console.log('Error:' + JSON.stringify(res))}
         })
     }
 )
 
-
-  
-  
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
-const express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser'),
-      { Client } = require('pg'),
-      multer = require('multer'),
-      jwt = require('jsonwebtoken'),
-      env = require('node-env-file'),
-      AWS = require('aws-sdk'),
-      upload = multer()
-      
+const 
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    { Client } = require('pg'),
+    multer = require('multer'),
+    jwt = require('jsonwebtoken'),
+    env = require('node-env-file'),
+    AWS = require('aws-sdk'),
+    upload = multer(),
+    bcrypt = require('bcrypt'),
+    uuidv4 = require('uuid/v4')
+
+
+
 env(__dirname + '/.env');
 
 const client = new Client(process.env.DB_CONNECT)
@@ -17,6 +22,9 @@ client.connect()
 const s3 = new AWS.S3()
 AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
 
+
+
+
 // ------------------APP.USE----------------------
 app.use(express.static('public'))
 app.use(bodyParser());
@@ -24,13 +32,12 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 // ------------------APP.GET----------------------
 app.get('/', (req, res) => {res.sendFile(__dirname + '/public/views/index.html');});
 app.get('/files', (req, res) => {res.sendFile(__dirname + '/public/views/files.html');});
 app.get('/signup', (req, res) => {res.sendFile(__dirname + '/public/views/signup.html');});
-
 app.get('/files/:username', (req, res)=>{
     const username = req.params.username
     const params2 = {
@@ -52,75 +59,120 @@ app.get('/files/:username', (req, res)=>{
 app.get('/user', (req,res) => {
     console.log('IN app.get ')
     // console.log(req)
-    const email = req.query.email, 
-          password = req.query.password 
+    const 
+    email = req.query.email, 
+    password = req.query.password 
 
     console.log(email)
     console.log(password)
 
     client.query(
-        'SELECT * FROM users WHERE email = $1',[email],
+        'SELECT * FROM users WHERE email = $1 and password = $2',
+            [email, password],
         (err,psql_res) => {
-            // console.log(psql_res)
-            if(psql_res.rows.length === 0) {
-                console.log('Email Does Not Exists: ' + email)
+            if (psql_res.rows.length === 0) {
+                console.log('Email or password is incorrect.')
                 res.status(400)
             } else {
-               //redirect to page with results from query
+                //redirect to page with results from query
                 console.log(psql_res.rows[0])
                 const user = {
                     id: psql_res.rows[0].user_id, 
                     username: psql_res.rows[0].username,
                     email: psql_res.rows[0].email
                 }
-               jwt.sign({user}, 'secretkey', { expiresIn: '12h'}, (err,token)=>{
-                res.json({
-                    token
-                })
-            });
-               console.log('redirect to profile page')
+                
+                jwt.sign({user}, 'secretkey', { expiresIn: '12h'}, (err,token) => {
+                    res.json({token})
+                    res.status(200)
+                });
+                console.log('redirect to profile page')
             }
         }
-     )
+    )
 })
 
 // ------------------APP.POST----------------------
 
 app.post('/user', (req,res)=>{
-    // console.log(req.body)
-    const username = req.body.username,
-          password = req.body.password,
-          email = req.body.email
- 
-    console.log('BEFORE INSERT')
+    const 
+        username = req.body.username,
+        password = req.body.password,
+        email = req.body.email 
+        console.log('username ' + username)   
+
+    bcrypt.hash(password, 10, (err, hash)=>{ 
         
-    client.query(
-        'SELECT * FROM users WHERE username = $1',[username],
-        (err,psql_res) => {
-            console.log(psql_res)
-            if(psql_res.rows.length!=0) {
-                console.log('Username Exists: ' + username)
-                res.status(400)
-            } else {
-                client.query(
-                    `INSERT INTO users (username, password, email)
-                     VALUES($1, $2, $3)`,
-                     [username, password, email],
-                     (err,psql_res) => {
-                        //  console.log(psql_res)
-                        //  console.log('query')
-                        res.status(200)
-                     }
-                 )
-            }
+        if (err){
+            res.status(500)
+            return
         }
-     )
+        client.query(
+            'SELECT * FROM users WHERE username = $1', 
+                [username],
+            (err,psql_res) => {
+                if(err){
+                    console.log('select err '+ err)
+                    res.status(500)
+                        return
+                }
+                console.log(psql_res)
+                if (psql_res.rows.length!=0) {
+                    console.log('Account already exists: ' + username)
+                    res.status(400)
+                } else {
+                    //use uuid for jwt token
+                    const user_id = uuidv4() 
+                        client.query(
+                            `INSERT INTO users (username, password, email, user_id) VALUES($1, $2, $3, $4)`,
+                                [username, hash, email, user_id],
+                            (err,psql_res) => {
+                                if (err) {
+                                    console.log('insert err' + JSON.stringify(err))
+                                    res.status(500)
+                                    return
+                                }
+                                const user = {
+                                    id: user_id, 
+                                    username,
+                                    email
+                                }
+                                jwt.sign({user}, 'secretkey', { expiresIn: '12h'}, (err,token)=>{
+                                    res.json({token})
+                                    res.status(200)
+                                });
+
+                                // client.query(`SELECT user_id FROM users WHERE email=$1`,
+                                //     [email],
+                                //     (err, psql_res)=>{
+                                //         if (err) {
+                                //             console.log('err' + JSON.stringify(err))
+                                //             res.status(500)
+                                //             return
+                                //         }
+                                //         const user = {
+                                //             id: psql_res.rows[0].user_id, 
+                                //             username: psql_res.rows[0].username,
+                                //             email: psql_res.rows[0].email
+                                //         }
+                                //         jwt.sign({user}, 'secretkey', { expiresIn: '12h'}, (err,token)=>{
+                                //             res.json({token})
+                                //         });
+                                //     }
+                                // )
+                                // console.log(psql_res)
+                                // //  console.log('query')
+                            }
+                        )
+                }
+            })
+        }
+    )
 })
 
 app.post('/files', upload.single('myfile'), (req, res) => {
     const name = req.file.originalname
     const data = req.file.buffer
-
     // TODO: add username instead of hardcode: const username = req.body.username
     const username = "Andrea"
     // console.log(req.body)
@@ -167,21 +219,21 @@ app.post('/account', verifyToken, (req,res) => {
     })
 })
 
-app.post('/login', (req, res) => {
-    //TODO: Mock User (make request to login - send username and password)
-    console.log("dgn ghn" +JSON.stringify(req.body))
+// app.post('/login', (req, res) => {
+//     //TODO: Mock User (make request to login - send username and password)
+//     console.log("dgn ghn" +JSON.stringify(req.body))
     
-    const user = {
-        id: 1, 
-        username: 'andrea',
-        email: 'andrea@example.com'
-    }
-    jwt.sign({user}, 'secretkey', { expiresIn: '12h'}, (err,token)=>{
-        res.json({
-            token
-        })
-    });
-})
+//     const user = {
+//         id: 1, 
+//         username: 'andrea',
+//         email: 'andrea@example.com'
+//     }
+//     jwt.sign({user}, 'secretkey', { expiresIn: '12h'}, (err,token)=>{
+//         res.json({
+//             token
+//         })
+//     });
+// })
 
 //verify token ** take with app.post ** 
 function verifyToken (req, res, next) {
