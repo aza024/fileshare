6b01e6debe989618afec7e1fa91453067453bfac
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-10-12 13:39:03 -0700

    return file data from s3

--- backend/public/scripts/files.js
+++ backend/public/scripts/files.js
@@ -27,21 +27,44 @@ if (loggedIn = true){
                 </div>
                 <div class = "fileDetail">
                   <h3>Filename: ${filename}</h3>
+                  <div class = "dlBtnAppend"></div>
                   <h3>Last Modified Date: ${modified}</h3>
                   <h3>Size: ${size} </h3>
                 </div>
               </div>`)
         }
 
-        $('.downbtn').on('click',(e)=>{
+        $('.dlBtnAppend').append(
+          `<button class="downBtn">Download</button>`
+        )
+        //refactor into seprate AJAX call or figure out how to get both
+        $('.downBtn').on('click',(e)=>{
           console.log('DOWNLOAD CLICKED')
+          $.ajax({
+            dataType: 'json',
+            method: 'GET',
+            url:`/files/admin/_atestfile.txt`,
+            // data:{
+            //       username, 
+            //       filename
+            //     },
+            success: (res)=>{
+              //get data field
+              console.log('Success '+ JSON.stringify(res))
+      
+            },
+            error:   (res) =>{
+              console.log('Error')
+            }
+        })
         })
     },
     error: (res)=>{
       console.log('Error: Unable to retrieve files ' + JSON.stringify(res))
     }
+}, //end ajax
 
-},
+
 // File Upload
   $('#uploadBtn').on('click',(e)=>{
       e.preventDefault();
@@ -59,6 +82,27 @@ if (loggedIn = true){
         }).fail(function(){
           console.log("An error occurred, the files couldn't be sent!");
         });
+  }),
+//get file content
+  $('.downBtn').on('click',(e)=>{
+    $.ajax({
+        dataType: 'json',
+        method: 'GET',
+        url:`/files/${username}/${filename}`,
+        data:{
+              username, 
+              filename
+            },
+        success: (res)=>{
+          console.log('Success '+ res)
+        },
+        error:   (res) =>{
+          console.log('Error')
+        }
+    })
   })
+
+
+// end logged in condition
 )}
 
