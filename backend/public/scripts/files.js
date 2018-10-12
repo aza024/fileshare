ff77c7f224864af55965438f19fffe9afe92edbc
Andrea  Piazza <andrea@Andreas-MacBook-Pro.local>
2018-10-12 11:15:29 -0700

    able to render S3 files on page. Added REGEX to get filenames and extensions from path

--- backend/public/scripts/files.js
+++ backend/public/scripts/files.js
@@ -8,21 +8,37 @@ if (loggedIn = true){
   $.ajax({
     dataType: 'json',
     method: 'GET',
-    // changed from /user
     url:`/listfiles/${username}`,
-    data:{
-        username
-    },
+    data:{username},
     success: (res)=>{
       let files = res.Contents
-      console.log(files)
       for(var i =0; i < res.Contents.length; i++){
-        console.log(files[i].LastModified)
-        modified = (files[i].LastModified)
+        let
+          modified = (files[i].LastModified),
+          key = (files[i].Key),
+          size = (files[i].Size),
+          filename = key.replace(/^.*[\\\/]/, '')
+          extension = filename.split('.').pop();
+          console.log(extension)
+
+          $('.filesInfo').append(
+            `<div class = "fileInfo"> 
+              <div class = "fileDetail"> 
+                <h1>${extension}</h1>
+              </div>
+                <h3>Filename: ${filename}</h3>
+                <h3>Last Modified Date: ${modified}</h3>
+                <h3>Size: ${size} </h3>
+            </div>`)
+
+        console.log(filename)
+        console.log(modified)
+        console.log(key)
+        console.log(size)
       }
     },
     error: (res)=>{
-      console.log('Error:' + JSON.stringify(res))
+      console.log('Error: Unable to retrieve files ' + JSON.stringify(res))
     }
 
 },
