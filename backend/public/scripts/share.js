76f1e2561c07bd843362e9658675bda11a2b2752
Andrea  Piazza <apiazza023@gmail.com>
2018-10-16 00:56:06 -0700

    share file styled - able to preivew and download

--- backend/public/scripts/share.js
+++ backend/public/scripts/share.js
@@ -71,7 +71,19 @@ getFileName = (url) => {
     const lastIndex2 = modifiedUrl.lastIndexOf('/')
     return modifiedUrl.substring(lastIndex2 + 1)
 
-} 
+}
+
+getUsername = (url) => {
+    const lastIndex = url.lastIndexOf('/')
+    const modifiedUrl = url.substr(0, lastIndex)
+
+    const lastIndex2 = modifiedUrl.lastIndexOf('/')
+    const modifiedUrl2 = modifiedUrl.substring(0, lastIndex2)
+
+
+    const lastIndex3 = modifiedUrl2.lastIndexOf('/')
+    return modifiedUrl2.substring(lastIndex3 + 1)
+}
 
 downloadfile = (username, filename, fileId, success, error) => {
     $.ajax({
@@ -88,21 +100,34 @@ const filename = getFileName(currentUrl)
 const extension = filename.split('.').pop()
 
 if (isImage(extension) || isVideo(extension)) { 
+
+    document.getElementById('previewDiv').innerHTML = ''
     let previewBtn = document.createElement('button')
     previewBtn.setAttribute('id', 'previewBtn')
 
     let previewBtnText = document.createTextNode('Preview')
 
     previewBtn.appendChild(previewBtnText)
+
+    // $(previewBtn).on('click',)
     
     document.getElementById('previewDiv').appendChild(previewBtn)
+
 }
 
+const username = getUsername(currentUrl)
+let textH3 = document.createElement(`h3`)
+let text = document.createTextNode(`${username} has shared ${filename} with you!`)
+
+textH3.appendChild(text)
+
+document.getElementById('shareText').appendChild(textH3)
 
 $(`#previewBtn`).on('click',()=>{
-    const username = localStorage.getItem('username')
+    console.log('clicked preview button')
+    
+    const username = getUsername(currentUrl)
 
-    const currentUrl = window.location.href.replace('shareFile', 'files');
     console.log(currentUrl)
     const filename = getFileName(currentUrl)
     const fileId = getFileId(currentUrl);
@@ -112,13 +137,12 @@ $(`#previewBtn`).on('click',()=>{
       fileId,
       (res) => {
         const extension = filename.split('.').pop()
+        document.getElementById('previewDiv').innerHTML = ''
 
         if (isImage(extension)) {
           previewImage(res.Body.data, filename, fileId)
-          $(`.${fileId}Wrapper`).hide()
         } else if (isVideo(extension)) {
           previewVideo(res.Body.data, filename, fileId)
-          $(`.${fileId}Wrapper`).hide()
         }
       },
       (res) => {console.log('Error')}
